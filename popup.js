1dd30c1ab5d7b612d2535772ec76a7e159927b48
drugmart <40585844+drugmart@users.noreply.github.com>
2024-05-28 22:59:43 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.getElementById('apply-coupons').addEventListener('click', () => {
+    alert('Applying coupons...');
+});
+
+document.getElementById('track-prices').addEventListener('click', () => {
+    alert('Tracking prices...');
+});
+
+document.getElementById('get-recommendations').addEventListener('click', () => {
+    alert('Fetching recommendations...');
+});
